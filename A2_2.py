9a0702b5bb8c02c3cf6c7535701181119799598b
Zach Estreito <zachestreito@gmail.com>
2023-10-09 18:34:50 -0700

    Done with question 2

--- A2_2.py
+++ A2_2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pgm import PGM
 from statistics import mean
+from scipy.ndimage import gaussian_filter
 
 # Average filter
 def average(image, size):
@@ -18,10 +19,25 @@ def average(image, size):
         image.pixels = temp_pixels
         image.save("_avg" + "_" + str(size))
 
+# Gaussian filter
+def gaussian(image, size):
+    if size % 2 == 0:
+        print("Error: size must be an odd integer")
+        return
+    else:
+        offset = int((size - 1) / 2)
+        np_pixels = np.array(image.pixels)
+        temp_pixels = (gaussian_filter(np_pixels.astype(int), sigma=(float(size)/5.0), radius=offset))
+        image.pixels = temp_pixels
+        image.save("_gaussian" + "_" + str(size))
 
 pgm = PGM("lenna")
 average(pgm, 7)
 average(pgm, 15)
+gaussian(pgm, 7)
+gaussian(pgm, 15)
 pgm = PGM("sf")
 average(pgm, 7)
 average(pgm, 15)
+gaussian(pgm, 7)
+gaussian(pgm, 15)

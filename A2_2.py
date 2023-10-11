1ec3c512d0305e0d63107f5385815e2453897862
Zach Estreito <zachestreito@gmail.com>
2023-10-10 20:27:42 -0700

    Fixed #4 to use handmade Gaussian filter

--- A2_2.py
+++ A2_2.py
@@ -1,7 +1,6 @@
 import numpy as np
 from pgm import PGM
 from statistics import mean
-from scipy.ndimage import gaussian_filter
 
 # Average filter
 def average(image, size):
@@ -14,7 +13,7 @@ def average(image, size):
         temp_pixels = np.empty_like(np_pixels)
         for x in range(len(image.pixels)):
             for y in range(len(image.pixels[x])):
-                flat_array = np_pixels[np.clip(x-offset, 0, 255) : np.clip(x+offset+1, 0, 255), np.clip(y-offset, 0, 255) : np.clip(y+offset+1, 0, 255)].astype(int).flatten()
+                flat_array = get_section(np_pixels, [x,y], offset).astype(int).flatten()
                 temp_pixels[x][y] = mean(flat_array)
         image.pixels = temp_pixels
         image.save("_avg" + "_" + str(size))
@@ -57,13 +56,14 @@ def get_section(arr, center, square_radius):
               center[1] - square_radius + lp:center[1] + square_radius + 1 + lp]
 
 
-pgm = PGM("lenna")
-average(pgm, 7)
-average(pgm, 15)
-gaussian(pgm, 7)
-gaussian(pgm, 15)
-pgm = PGM("sf")
-average(pgm, 7)
-average(pgm, 15)
-gaussian(pgm, 7)
-gaussian(pgm, 15)
+average(PGM("lenna"), 7)
+average(PGM("lenna"), 15)
+
+gaussian(PGM("lenna"), 7)
+gaussian(PGM("lenna"), 15)
+
+average(PGM("sf"), 7)
+average(PGM("sf"), 15)
+
+gaussian(PGM("sf"), 7)
+gaussian(PGM("sf"), 15)
--- A2_4.py
+++ A2_4.py
@@ -1,7 +1,42 @@
 import numpy as np
 from pgm import PGM
 from statistics import mean
-from scipy.ndimage import gaussian_filter
+
+# Gaussian filter
+def gaussian(image, size):
+    if size % 2 == 0:
+        print("Error: size must be an odd integer")
+        return
+    else:
+        # Set radius and sigma
+        offset = int((size - 1) / 2)
+        sigma = (float(size) / 5.0)
+        # Create guassian mask with a sum of 1.0
+        gauss_matrix = gaussian_matrix(size, sigma)
+        np_pixels = np.array(image.pixels)
+        temp_pixels = np.zeros((256,256))
+        for x in range(len(image.pixels)):
+            for y in range(len(image.pixels[x])):
+                temp_array = get_section(np_pixels, [x,y], offset)
+                temp_pixels[x][y] = int(sum(sum(temp_array.astype(float) * gauss_matrix)))
+        return temp_pixels.astype(int)
+
+# Create gaussian mask with a sum of 1.0
+def gaussian_matrix(size, sigma):
+    axis = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
+    gauss = np.exp(-0.5 * np.square(axis) / np.square(sigma))
+    kernel = np.outer(gauss, gauss)
+    return kernel / np.sum(kernel)
+
+# Get section of array and pad edge values
+def get_section(arr, center, square_radius):
+    tp = max(0, -(center[0] - square_radius))
+    bp = max(0, -((arr.shape[0]-center[0]-1) - square_radius))
+    lp = max(0, -(center[1] - square_radius))
+    rp = max(0, -((arr.shape[1]-center[1]-1) - square_radius))
+    arr = np.pad(arr, [[tp, bp], [lp, rp]], 'edge')
+    return arr[center[0] - square_radius + tp:center[0] + square_radius + 1 + tp, \
+              center[1] - square_radius + lp:center[1] + square_radius + 1 + lp]
 
 # Unsharp mask
 def high_boost(image, k):
@@ -9,19 +44,17 @@ def high_boost(image, k):
     size = 7
     np_pixels = np.array(image.pixels)
     np_pixels = np_pixels.astype(int)
-    lp_pixels = (gaussian_filter(np_pixels, sigma=(float(size)/5.0), radius=offset))
+    lp_pixels = gaussian(image, size)
     mask = np_pixels - lp_pixels
     image.pixels = np.clip((np_pixels + (k * mask).astype(int)), 0, 255)
     image.save("_boost" + "_k" + str(k))
 
-pgm = PGM("lenna")
-high_boost(pgm, 1.0)
-high_boost(pgm, 1.5)
-high_boost(pgm, 2.0)
+high_boost(PGM("lenna"), 1.0)
+high_boost(PGM("lenna"), 1.3)
+high_boost(PGM("lenna"), 1.6)
 
-pgm = PGM("f_16")
-high_boost(pgm, 1.0)
-high_boost(pgm, 1.5)
-high_boost(pgm, 2.0)
+high_boost(PGM("f_16"), 1.0)
+high_boost(PGM("f_16"), 1.3)
+high_boost(PGM("f_16"), 1.6)
 
-# Note: lenna looks best with k=1.0, f_16 looks best with k=1.5
+# Note: They both look best with k=1.0
